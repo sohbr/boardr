d7aa999de6d083dc5b0175dc3a107b106a81f431
sohbr <sohnbrian@gmail.com>
2017-09-24 10:13:10 -0700

    Add awesome font CSS

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBarContainer from "./navbar/navbar_container";
 import { Route } from "react-router-dom";
 import UploadForm from "./upload_form";
+import Footer from "./footer.jsx";
 
 import PhotoIndexContainer from "./photos/photo_index_container";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
@@ -14,6 +15,9 @@ const App = () => (
     <section>
       <ProtectedRoute path="/users/:userId" component={PhotoIndexContainer} />
     </section>
+    <footer>
+      <Footer />
+    </footer>
   </div>
 );
 
--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,23 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="portfolio-link">
-        <a href="http://wwww.samwang.io">
-          <i className="fa fa-home" />
-        </a>
-      </div>
       <div className="github-link">
-        <a href="https://github.com/samwzz/aperture">
+        <a href="https://github.com/sohbr/boardr">
           <i className="fa fa-github" />
         </a>
       </div>
-      <div className="linkedin-link">
-        <a href="https://www.linkedin.com/in/sam-wang-665980139/">
-          <i className="fa fa-linkedin" />
-        </a>
-      </div>
       <div className="email-link">
-        <a href="mailto:sam.zz.wang@gmail.com">
+        <a href="mailto:sohnbrian@gmail.com">
           <i className="fa fa-envelope" />
         </a>
       </div>
