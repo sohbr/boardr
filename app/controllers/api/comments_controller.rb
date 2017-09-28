ae54f2fada5637f9795974a620526985d3bbd010
sohbr <sohnbrian@gmail.com>
2017-09-28 15:57:40 -0700

    Add comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,46 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(photo_id: params[:photo_id])
+  end
+
+  def show
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update_attributes(comment_params)
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    if @comment.destroy
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :photo_id)
+  end
+
+end
